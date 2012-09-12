f4fb0ec8027eb9713208ef59d8397697a79c23ab
Christian Toftegaard <cm.toftegaard@stud.uis.no>
2012-09-12 04:09:38 +0200

    Testing

---
+++ app/models/Task.java
@@ -0,0 +1,33 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import play.db.ebean.*;
+import play.data.validation.Constraints.*;
+
+@Entity
+public class Task extends Model{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	public Long id;
+
+	@Required
+	public String label;
+
+	public static Finder<Long,Task> find = new Finder<Long,Task>(Long.class, Task.class);
+
+	public static List<Task> all() {
+		return find.all();
+	}
+
+	public static void create(Task task) {
+		task.save();
+	}
+
+	public static void delete(Long id) {
+		find.ref(id).delete();
+	}
+
+}
